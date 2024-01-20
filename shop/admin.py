108f9f1ebb1d093a3b1eb26ff087a9e6f3344427
yousof <yosofasady2@gmail.com>
2024-01-20 13:15:56 +0330

    make admins

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,106 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from . import models
 
-# Register your models here.
+class InventoryFilter(admin.SimpleListFilter):
+    title = _('Inventory')
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', _('Low')),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = [_('Clear inventory')]
+    list_display = [_('Title'), _('Unit Price'),
+                    _('Inventory Status'), _('Collection Title')]
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    list_select_related = ['collection']
+    search_fields = ['title']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return _('Low')
+        return _('OK')
+
+    @admin.action(description=_('Clear inventory'))
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            _('{count} products were successfully updated.').format(count=updated_count),
+            messages.ERROR
+        )
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['featured_product']
+    list_display = [_('Title'), _('Products Count')]
+    search_fields = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+                reverse('admin:store_product_changelist')
+                + '?'
+                + urlencode({'collection__id': str(collection.id)}))
+        return format_html('<a href="{}">{} {}</a>', url, collection.products_count, _('Products'))
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [_('First Name'), _('Last Name'), _('Membership'), _('Orders')]
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+                reverse('admin:store_order_changelist')
+                + '?'
+                + urlencode({'customer__id': str(customer.id)}))
+        return format_html('<a href="{}">{} {}</a>', url, customer.orders_count, _('Orders'))
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = [_('ID'), _('Placed At'), _('Customer')]
--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tag
+from django.utils.translation import gettext_lazy as _
+
 
 # Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = [_('label')]
