8cfc2d977491b13c309cd8bfb270a8cbaea6711a
yousof <yosofasady2@gmail.com>
2024-01-20 13:08:12 +0330

    translation for tags

--- tags/models.py
+++ tags/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils.translation import gettext_lazy as _
 
-
-# Create your models here.
 
 class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
@@ -15,15 +14,23 @@ class TaggedItemManager(models.Manager):
 
 
 class Tag(models.Model):
-    label = models.CharField(max_length=255)
+    label = models.CharField(_("Label"), max_length=255)
 
     def __str__(self) -> str:
         return self.label
 
+    class Meta:
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
+
 
 class TaggedItem(models.Model):
     objects = TaggedItemManager()
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name=_("Tag"))
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+
+    class Meta:
+        verbose_name = _("Tagged Item")
+        verbose_name_plural = _("Tagged Items")
