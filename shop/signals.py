517702e8b49637a645033a81dbbae9e839a48c8c
yousof <yosofasady2@gmail.com>
2024-02-10 22:29:15 +0330

    adding signal to catch user for cart

--- shop/signals.py
+++ shop/signals.py
@@ -1,9 +1,12 @@
+import inspect
 from time import sleep
 
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import Customer, Order, Transaction, OrderItem
+
+from core.models import User
+from .models import Customer, Order, Transaction, OrderItem, CartItem, Cart
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -37,3 +40,26 @@ def update_total_sales_of_transaciton(sender, instance, created, **kwargs):
     total_price = order.get_total_price()
     transaction.total_price = total_price
     transaction.save()
+
+
+@receiver(post_save, sender=CartItem)
+def update_cart_customer(sender, instance, created, **kwargs):
+    cart_id = instance.cart.id
+    request = None
+    for frame_record in inspect.stack():
+        if frame_record[3] == 'get_response':
+            request = frame_record[0].f_locals['request']
+            """https://docs.python.org/3/library/inspect.html"""
+            """https://www.geeksforgeeks.org/inspect-module-in-python/"""
+            break
+    print('request', request)
+    if request:
+        user = request.user
+        if not isinstance(user, User):
+            user = None
+    else:
+        user = None
+    print(user.id)
+    cart = Cart.objects.get(id=cart_id)
+    cart.customer = Customer.objects.get(user_id=user.id)
+    cart.save()
