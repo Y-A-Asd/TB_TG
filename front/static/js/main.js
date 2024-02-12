2eef31835670aa82fc62f874c3068556036df958
yousof <yosofasady2@gmail.com>
2024-02-12 22:36:48 +0330

    feat:adding filtering base on price

--- front/static/js/main.js
+++ front/static/js/main.js
@@ -255,14 +255,14 @@ $(document).ready(function(){
         noUiSlider.create(nonLinearSlider, {
             connect: true,
             behaviour: 'tap',
-            start: [ 500, 4000 ],
+            start: [ 0, 100000 ],
             range: {
                 // Starting at 500, step the value by 500,
                 // until 4000 is reached. From there, step by 1000.
                 'min': [ 0 ],
                 '10%': [ 500, 500 ],
-                '50%': [ 4000, 1000 ],
-                'max': [ 10000 ]
+                '50%': [ 40000, 5000 ],
+                'max': [ 10000000 ]
             }
         });
 
