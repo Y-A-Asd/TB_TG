ce6e4391ca52214e88712edac44b116fa00e4bc6
yousof <yosofasady2@gmail.com>
2024-03-06 20:43:56 +0330

    fix: nginx fixed and collectstatics

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,11 @@ echo "Apply database migrations"
 python manage.py migrate
 
 echo "Collecting Static Files"
-python manage.py collectstatic
+#python manage.py collectstatic
 
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
+#test
+
+#gunicorn demo.wsgi:application --bind 0.0.0.0:8000
