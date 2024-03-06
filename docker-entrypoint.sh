d1832f3b1d0c16e7d62f0b65a3d6290c28e33259
yousof <yosofasady2@gmail.com>
2024-03-06 12:30:04 +0330

    feat: work with env and adding nginx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,9 @@
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Collecting Static Files"
+python manage.py collectstatic
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
