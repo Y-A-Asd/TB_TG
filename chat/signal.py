1a8a6e5dda801cc2a6c8c4d205801b9b477292e7
yousof <yosofasady2@gmail.com>
2024-10-08 02:00:09 +0330

    working on websocket

--- chat/signal.py
+++ chat/signal.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from .models import Message
 
 
-@receiver(post_save, sender=Message)
-def delete_even_id_message(sender, instance, created, **kwargs):
-    if created and instance.id % 2 == 0:
-        instance.delete()
+# @receiver(post_save, sender=Message)
+# def delete_even_id_message(sender, instance, created, **kwargs):
+#     if created and instance.id % 2 == 0:
+#         instance.delete()
